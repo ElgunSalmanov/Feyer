74b1086ae5ce8a7e7e1106ccb8ade41267111e80
Faig Haci <elgun.s@coders.edu.az>
2024-04-23 16:46:38 +0400

    mobile version

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -2,19 +2,18 @@ import { NavLink, useLocation } from "react-router-dom";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { FaCaretDown } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Basket from "../../components/basket/Basket";
 import "./header.scss";
-import { useSelector } from "react-redux";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isBlock, setIsBlock] = useState(false);
   const body = document.querySelector("body");
   const location = useLocation();
   const card = useSelector((state) => state);
-
-  const handleClick = () => {
-    setIsOpen(true);
-  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -24,16 +23,27 @@ function Header() {
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    setIsMenuOpen(false);
   }, [location.pathname]);
 
   return (
     <nav className="navbar">
+      <GiHamburgerMenu
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="navbar-hamburger"
+      />
       <NavLink to="/" className="navbar-home">
         Feyer
       </NavLink>
-      <ul className="navbar-list">
+      <ul
+        className="navbar-list"
+        style={{
+          opacity: isMenuOpen ? 1 : 0,
+        }}
+      >
         <NavLink
           to="/shop"
+          onClick={() => setIsBlock(true)}
           className={({ isActive }) =>
             isActive
               ? "navbar-list-item first-child active"
@@ -43,7 +53,10 @@ function Header() {
           Shop
           <FaCaretDown className="down" />
         </NavLink>
-        <div className="navbar-popup">
+        <div
+          className="navbar-popup"
+          style={{ display: isBlock ? "flex" : "none" }}
+        >
           <NavLink to="/jackets" className="navbar-popup-item">
             Jackets
           </NavLink>
@@ -63,7 +76,7 @@ function Header() {
       </ul>
       <div className="navbar-shopping">
         <MdOutlineShoppingBag
-          onClick={handleClick}
+          onClick={() => setIsOpen(true)}
           className="navbar-shopping-icon"
         />
         <p className="navbar-shopping-count">({card["name"].card.length})</p>
